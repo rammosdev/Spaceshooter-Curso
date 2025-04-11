4331bd3d61aa878df9eca10025b7f42e76b0bada
rammosz <88007473+rammosdev@users.noreply.github.com>
2025-04-11 19:11:20 +0200

    Script de Movimentação + Sprites

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimentação();
+    }
+
+    public void Movimentação()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        Vector2 mySpeed = new Vector2(horizontal, vertical) * speed;
+        rb.linearVelocity = mySpeed;
+    }
+}

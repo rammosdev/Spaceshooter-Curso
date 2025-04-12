a1dd6618035fefabf9749547831c145bf0b8534d
rammosz <88007473+rammosdev@users.noreply.github.com>
2025-04-12 22:47:36 +0200

    Inimigo aparecendo na tela.

--- Assets/Scripts/InimigoController.cs
+++ Assets/Scripts/InimigoController.cs
@@ -7,27 +7,41 @@ public class InimigoController : MonoBehaviour
     public GameObject bullet;
     public GameObject tiroPos;
     private float waitShoot = 1f;
+    private SpriteRenderer sprite;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.linearVelocity = new Vector2(0f, -speed);
+        waitShoot = Random.Range(0.5f, 2f);
+        sprite = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Atirando();
+
+
     }
 
     void Atirando()
     {
-        waitShoot -= Time.deltaTime;
-        if (waitShoot <= 0)
+
+        bool visivel = sprite.isVisible;
+        Debug.Log(visivel);
+        if (visivel == true)
         {
-            Instantiate(bullet, tiroPos.transform.position, tiroPos.transform.rotation);
-            waitShoot = 1f;
+            //Diminuindo a espera pelo deltaTime
+            waitShoot -= Time.deltaTime;
+            if (waitShoot <= 0)
+            {
+                Instantiate(bullet, tiroPos.transform.position, tiroPos.transform.rotation);
+                waitShoot = Random.Range(1.5f, 2f);
+            }
         }
+
 
     }
 }

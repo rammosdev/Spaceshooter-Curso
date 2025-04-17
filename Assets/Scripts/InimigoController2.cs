05f2f96ffff04f321a80b0fb1584dce625b0bb8d
rammosz <88007473+rammosdev@users.noreply.github.com>
2025-04-17 13:52:22 +0200

    Inimigo2 movimentação

--- Assets/Scripts/InimigoController2.cs
+++ Assets/Scripts/InimigoController2.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class InimigoController2 : InimigoPai
 {
+    Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = new Vector2(0f, -speed);
     }
 
     // Update is called once per frame

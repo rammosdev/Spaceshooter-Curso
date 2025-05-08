900edcd167af333f79979bfedadb2255851f9155
rammosz <88007473+rammosdev@users.noreply.github.com>
2025-05-08 23:02:25 +0200

    Estado1 atirando.

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -1,13 +1,19 @@
 using System;
 using UnityEngine;
 
-public class BossController : MonoBehaviour
+public class BossController : InimigoPai
 {
     [SerializeField] private string estado = "estado1";
-    [SerializeField] private float speed;
     private Rigidbody2D rb;
     [SerializeField] private bool direita;
     [SerializeField] private float limiteH;
+
+    [Header("Tiros")]
+    [SerializeField] private Transform posicaoTiro1;
+    [SerializeField] private Transform posicaoTiro2;
+    [SerializeField] private Transform posicaoTiro3;
+    [SerializeField] private GameObject tiro1;
+    [SerializeField] private GameObject tiro2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,6 +27,7 @@ public class BossController : MonoBehaviour
         {
             case "estado1":
                 Estado1();
+                Tiro1();
                 break;
             case "estado2":
                 Debug.Log("Estou no segundo estado");
@@ -51,5 +58,25 @@ public class BossController : MonoBehaviour
         {
             direita = true;
         }
+    }
+
+    private void Tiro1()
+    {
+        waitShoot -= Time.deltaTime;
+        if (waitShoot <= 0)
+        {
+            GameObject tiroUm = Instantiate(tiro1, posicaoTiro1.position, transform.rotation);
+            GameObject tiroDois = Instantiate(tiro1, posicaoTiro2.position, transform.rotation);
+
+            tiroUm.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0f, bulletSpeed);
+            tiroDois.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0f, bulletSpeed);
+            waitShoot = 1;
+        }
+
+    }
+
+    private void Tiro2()
+    {
+
     }
 }
